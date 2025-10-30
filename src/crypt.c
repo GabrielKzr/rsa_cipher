29e4f6665efbfc02769fddb46782c87436ef9c88
Gabriel Kaizer de Menezes <g.menezes@edu.pucrs.br>
2025-10-29 23:53:33 -0300

    test: organizing usage of import & export

--- src/crypt.c
+++ src/crypt.c
@@ -2,6 +2,13 @@
 
 // AINDA PRECISA SER IMPLEMENTADO
 
+static void crypt_eq(mpz_t x, mpz_t e, mpz_t m) {
+
+
+
+
+}
+
 int encrypt(const char* filepath_in, const char* filepath_out, const mpz_t e, const mpz_t n) {
     
     if (!filepath_in || !filepath_out) return -1;
@@ -13,17 +20,32 @@ int encrypt(const char* filepath_in, const char* filepath_out, const mpz_t e, co
     if (!fout) { fclose(fin); return -3; }
 
     uint8_t *buf = malloc(MAX_FRAMES);
+    uint8_t *buf2 = malloc(MAX_FRAMES);
     if (!buf) { fclose(fin); fclose(fout); return -4; }
 
     size_t r;
 
     printf("começando...\n");
 
+    memset(buf, 0, MAX_FRAMES);
+    memset(buf2, 0, MAX_FRAMES);
+
+    mpz_t x;
+    mpz_init(x);
+
     while ((r = fread(buf, 1, MAX_FRAMES, fin)) > 0) {
 
         // TODO: substituir este fwrite pela encriptação real
 
         printf("valor do r -> %zu\n", r);
+
+        // printf("%c\n", buf[0]);
+
+        mpz_import(x, r, -1, 1, 0, 0, buf);
+        
+        mpz_export(buf2, NULL, -1, 1, 0, 0, x);
+
+        printf("buf2 -> %s\n", (char*)buf2);
 
         if (fwrite(buf, 1, r, fout) != r) {
             free(buf);
@@ -32,7 +54,8 @@ int encrypt(const char* filepath_in, const char* filepath_out, const mpz_t e, co
             return -5;
         }
     }
-    
+
+    free(buf2);
     return 0;
 }
 
